53874fa5c4cb45d25e1b89b9e4230c2197789f05
CF12 <brianx0011@gmail.com>
2018-08-26 16:39:34 -0400

    Updated install script

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay
 
+# Install dependencies & packages
+yay -Sy --noconfirm i3 i3-gaps xorg polybar feh python-pywal rofi zsh oh-my-zsh-git nvim rxvt-unicode termite neovim thunar compton xcwd-git betterlockscreen playerctl pulseaudio arc-gtk-theme arc$
+
 
